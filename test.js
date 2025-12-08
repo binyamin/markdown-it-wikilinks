bb3ec39a5303e59f25015861b558d6444a44e72e
Binyamin Aron Green <binyamingreen@protonmail.com>
2025-12-08 15:00:15 -0500

    tests: move from "ava" to "node:test"

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
-import test from 'ava';
+import assert from 'node:assert/strict';
+import test from 'node:test';
 import markdownIt from 'markdown-it';
 import markdownItWikilinks from './index.js';
 
-test.before((t) => {
-	t.context.md = new markdownIt({ linkify: true }).use(markdownItWikilinks, {
-		base: '/notes/',
-	});
+const md = new markdownIt({ linkify: true });
+md.use(markdownItWikilinks, {
+	base: '/notes/',
 });
 
-test('works with emoji', (t) => {
-	let res = t.context.md.renderInline('[[📦]]');
+test('works with emoji', () => {
+	const res = md.renderInline('[[📦]]');
 	// Note: markdown-it url-encodes emoji symbols
-	t.assert(res === `<a href="/notes/%F0%9F%93%A6">📦</a>`, res);
+	assert.equal(res, `<a href="/notes/%F0%9F%93%A6">📦</a>`, res);
 });
 
-test('works with just note name', (t) => {
-	let res = t.context.md.renderInline('[[name]]');
-	t.assert(res === `<a href="/notes/name">name</a>`);
+test('works with just note name', () => {
+	const res = md.renderInline('[[name]]');
+	assert.equal(res, `<a href="/notes/name">name</a>`);
 });
 
-test('works also with custom text', (t) => {
-	let res = t.context.md.renderInline('[[name|text 📦]]');
-	t.assert(res === `<a href="/notes/name">text 📦</a>`);
+test('works also with custom text', () => {
+	const res = md.renderInline('[[name|text 📦]]');
+	assert.equal(res, `<a href="/notes/name">text 📦</a>`);
 });
